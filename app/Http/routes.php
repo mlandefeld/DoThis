01d9bee1a8228e1325dba57237edfa73740d90ee
Margaret Landefeld <mlandefeld@gmail.com>
2016-04-02 11:03:25 -0400

    First commit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 /*
 |--------------------------------------------------------------------------
@@ -27,5 +25,5 @@ Route::get('/', function () {
 */
 
 Route::group(['middleware' => ['web']], function () {
-    //
+    Route::get('/', ['as' => 'home', 'uses' => 'TaskController@index'] );
 });
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,5 @@
+@extends('templates.onecolumn')
+
+@section('content')
+
+@stop
